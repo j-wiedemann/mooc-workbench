8131f10e29eda3d5b551f34342b05df2175843f3
Jonathan Wiedemann <contact@freecad-france.com>
2019-05-25 19:16:25 +0200

    v0.1.0c

--- InitGui.py
+++ InitGui.py
@@ -35,7 +35,7 @@ import FreeCAD as app
 import FreeCADGui as gui
 
 global MOOC_VERSION
-MOOC_VERSION = 'V0.1.0b'
+MOOC_VERSION = 'V0.1.0c'
 
 moocWBpath = os.path.dirname(moocwb_locator.__file__)
 moocWBpath_medias = os.path.join(moocWBpath, 'medias')
